3010b9b3d0f8ad7841cc39b78ec6afff38a39ffa
Artur Sharafutdinov <nouriplai@yandex.com>
2021-12-07 23:22:26 +0300

    More constexpr qualifiers

--- pwcard.cpp
+++ pwcard.cpp
@@ -7,7 +7,7 @@ PwCard PwCard::generate(seed_type seed, int options)
 
     linear_congruential_engine g(seed);
 
-    static const std::array<symbol_type, column_count> init_header = {
+    static constexpr std::array<symbol_type, column_count> init_header = {
         0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87,
         0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F,
         0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97,
@@ -16,7 +16,7 @@ PwCard PwCard::generate(seed_type seed, int options)
     result.header = init_header;
     shuffle(result.header.begin(), result.header.end(), g);
 
-    static const std::array<symbol_type, 76> init_symbols = {
+    static constexpr std::array<symbol_type, 76> init_symbols = {
         0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
         0x38, 0x39,
               0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67,
