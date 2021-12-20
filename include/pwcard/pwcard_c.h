bf2fcc76afbd27e6d7d6e619a363e356057ddb7d
Artur Sharafutdinov <nouriplai@yandex.com>
2021-12-20 22:47:07 +0300

    C API header

---
+++ include/pwcard/pwcard_c.h
@@ -0,0 +1,11 @@
+#ifndef PWCARD_C_H
+#define PWCARD_C_H
+
+#include <stdint.h>
+
+#define PWCARD_COLUMN_COUNT 29
+#define PWCARD_ROW_COUNT 8
+
+typedef uint8_t pwcard_symbol_t;
+
+#endif // PWCARD_C_H
