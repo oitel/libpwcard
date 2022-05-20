26d1d7cb2c01d80f125869455e61d9c091f6cbf9
Artur Sharafutdinov <olribre@outlook.com>
2022-05-20 12:41:15 +0300

    Example app

---
+++ example/main.cpp
@@ -0,0 +1,27 @@
+#include <pwcard/pwcard.h>
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1) {
+        std::cerr << "Usage: pwcard NUMBER" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    PwCard::seed_type seed;
+    try {
+        seed = std::stoull(argv[1], nullptr, 16);
+    } catch (const std::exception &exc) {
+        std::cerr << exc.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    const auto options = PwCard::none;
+    const auto pwcard = PwCard::generate(seed, options);
+    std::cout << PwCard::to_utf8(pwcard.header) << std::endl;
+    for (auto &&row : pwcard.rows)
+        std::cout << PwCard::to_utf8(row) << std::endl;
+
+    return EXIT_SUCCESS;
+}
